85ab4e6c17c3fbe89bacc49680ed50ae2109c3dc
xfjin <xfjin@iflytek.com>
2019-05-05 17:34:24 +0800

    修改配置

---
+++ app/conf/config.js
@@ -0,0 +1,123 @@
+'use strict';
+// Template version: 1.3.1
+// see http://vuejs-templates.github.io/webpack for documentation.
+
+const path = require('path');
+
+module.exports = {
+  //开发环境
+  dev: {
+    // Paths
+    assetsSubDirectory: 'static', //指的是静态资源文件夹，默认“static”
+    assetsPublicPath: '/', //指的是发布路径
+    proxyTable: {
+      '/api': {
+        target: 'https://pcitest.zhixue.com/', //http://172.31.7.83:8066/',//https://pcitest.zhixue.com/
+        secure: false,
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': '/'
+        }
+      }
+    },
+
+    // Various Dev Server settings
+    host: '0.0.0.0', // can be overwritten by process.env.HOST
+    port: 9080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    autoOpenBrowser: false, //是否自动打开浏览器
+    errorOverlay: true, // 查询错误
+    notifyOnErrors: true, // 通知错误
+    poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
+    // 是跟devserver相关的一个配置，webpack为我们提供的devserver是可以监控文件改动的，但在有些情况下却不能工作，我们可以设置一个轮询（poll）来解决
+
+    // Use Eslint Loader?
+    // If true, your code will be linted during bundling and
+    // linting errors and warnings will be shown in the console.
+    useEslint: false, //是否使用eslint
+    // If true, eslint errors and warnings will also be shown in the error overlay
+    // in the browser.
+    showEslintErrorsInOverlay: false, //是否展示eslint的错误提示
+
+    /**
+     * Source Maps
+     */
+
+    // https://webpack.js.org/configuration/devtool/#development
+    devtool: 'cheap-module-eval-source-map', //webpack提供的用来方便调试的配置，它有四种模式
+
+    // If you have problems debugging vue-files in devtools,
+    // set this to false - it *may* help
+    // https://vue-loader.vuejs.org/en/options.html#cachebusting
+    cacheBusting: true, //一个配合devtool的配置，当给文件名插入新的hash导致清楚缓存时是否生成souce maps，默认在开发环境下为true
+
+    cssSourceMap: true //是否开启cssSourceMap
+
+  },
+
+  //线上环境
+  build: {
+    // Template for index.html
+    //编译后index.html的路径，path.resolve(__dirname, '../dist')中
+    index: path.resolve(__dirname, '../dist/index.html'),
+
+    // Paths
+    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsSubDirectory: 'static', //指的是静态资源文件夹，默认“static”
+    assetsPublicPath: 'http://www.zhixue.com/appTeachSupervision/', //指的是发布路径
+
+    /**
+     * Source Maps
+     */
+
+    productionSourceMap: false, //是否开启source-map
+    // https://webpack.js.org/configuration/devtool/#production
+    devtool: '#source-map',
+
+    // Gzip off by default as many popular static hosts such as
+    // Surge or Netlify already gzip all static assets for you.
+    // Before setting to `true`, make sure to:
+    // npm install --save-dev compression-webpack-plugin
+    productionGzip: false, //是否压缩
+    productionGzipExtensions: ['js', 'css'], //gzip模式下需要压缩的文件的扩展名，设置后会对相应扩展名的文件进行压缩
+
+    // Run the build command with an extra argument to
+    // View the bundle analyzer class_report after build finishes:
+    // `npm run build --class_report`
+    // Set to `true` or `false` to always turn it on or off
+    bundleAnalyzerReport: process.env.npm_config_report //是否开启打包后的分析报告
+  },
+
+  //测试环境
+  buildtest: {
+    // Template for index.html
+    //编译后index.html的路径，path.resolve(__dirname, '../dist')中
+    index: path.resolve(__dirname, '../dist/index.html'),
+
+    // Paths
+    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsSubDirectory: 'static', //指的是静态资源文件夹，默认“static”
+    assetsPublicPath: 'https://test.zhixue.com/appTeachSupervision/', //指的是发布路径
+
+    /**
+     * Source Maps
+     */
+
+    productionSourceMap: false, //是否开启source-map
+    // https://webpack.js.org/configuration/devtool/#production
+    devtool: '#source-map',
+
+    // Gzip off by default as many popular static hosts such as
+    // Surge or Netlify already gzip all static assets for you.
+    // Before setting to `true`, make sure to:
+    // npm install --save-dev compression-webpack-plugin
+    productionGzip: false, //是否压缩
+    productionGzipExtensions: ['js', 'css'], //gzip模式下需要压缩的文件的扩展名，设置后会对相应扩展名的文件进行压缩
+
+    // Run the build command with an extra argument to
+    // View the bundle analyzer class_report after build finishes:
+    // `npm run build --class_report`
+    // Set to `true` or `false` to always turn it on or off
+    bundleAnalyzerReport: process.env.npm_config_report //是否开启打包后的分析报告
+  }
+
+};
--- app/conf/webpack.base.conf.js
+++ app/conf/webpack.base.conf.js
@@ -3,14 +3,26 @@
 require('./check-versions')();
 
 const path = require('path');
-const config = require('../config');
+const config = require('./config');
 
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
 }
 
 module.exports = {
-  
+  output: {
+    path: config.build.assetsRoot,
+    filename: '[name].js',
+    publicPath:(() => {
+      if(process.env.NODE_ENV === 'production'){
+        return config.build.assetsPublicPath
+      } else if(process.env.NODE_ENV === 'test'){
+        return config.buildtest.assetsPublicPath
+      } else {
+        return config.dev.assetsPublicPath
+      }
+    })()
+  },
   resolve: {
     extensions: ['.js', '.vue', '.json'],
     alias: {
