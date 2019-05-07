1311a96cdb6badfface4710294210251f8e2d120
xfjin <xfjin@iflytek.com>
2019-05-07 14:28:25 +0800

    测试构建

--- vue_app/conf/webpack.prd.conf.js
+++ vue_app/conf/webpack.prd.conf.js
@@ -4,7 +4,7 @@ const merge = require('webpack-merge');
 const baseWebpackConfig = require('./webpack.base.conf');
 
 const webpackConfig = merge(baseWebpackConfig, {
-  	publicPath: 'https://sven820.github.io/gogo/'
+  	publicPath: 'https://sven820.github.io/gogo/dist/'
 });
 
 module.exports = webpackConfig;
